6cd60e337b0d5bd5eb255af80a60baeabb303a2e
Vinicius Kendi Takeushi <vinicius.takeushi@outlook.com>
2018-07-17 09:02:34 -0300

    Subindo ara o git hub

---
+++ TesteConsoleApp/TesteConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste subindo para o github");
+
+            Console.ReadKey();
+        }
+    }
+}
